c47253a44b775f64f9fbbdcae0be38b3429efa0b
pikaboo <stevinjack1ina1000@gmail.com>
2023-03-27 15:30:50 +0630

    changed a bit

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ import movies from "./api/movies.route.js";
 dotenv.config();
 
 const app = express(),
-    PORT = process.env.PORT || 5000;
+  PORT = process.env.PORT || 5000;
 app.use(cors());
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-}));
+  })
+);
 app.use(express.json());
 app.use("/api/v1/movies", movies);
 app.use("*", (req, res) => {
-    res.status(404).json({
-        error: "Not Found!"
-    });
+  res.status(404).json({
+    error: "Not Found!",
+  });
 });
 
 export default app;
